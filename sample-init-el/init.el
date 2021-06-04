b1ef1c8fd92bb940f89f34d10715ab3a06945a8b
Bob Yantosca <yantosca@seas.harvard.edu>
2021-06-04 09:27:58 -0400

    Now define a switch for emacs with the athena-X11 backend
    This switch (enable-athena-backend) is defined in the init.el, and
    is used to disable loading fonts.  When using the Athena backend,
    fonts (I think) must be loaded via .Xresources.
    
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- sample-init-el/init.el
+++ sample-init-el/init.el
@@ -3,6 +3,13 @@
 ;; 28 May 2021 -- Bob Yantosca -- yantosca@seas.harvard.edu
 
 ;;=============================================================================
+;; Set variables to toggle various options on (t) or off (nil)
+;;=============================================================================
+(setq enable-two-vertical-frames t)   ;; Use larger window w/ 2 vertical frames
+(setq enable-vterm nil)               ;; Use vterm instead of term
+(setq enable-athena-backend nil)      ;; Use athena/X11 instead of gtk
+
+;;=============================================================================
 ;; FONTS - customize to look best on your system!
 ;;
 ;; Some good options (note: 120 = 12pt, etc)
@@ -12,15 +19,10 @@
 ;; (4) "-*-Lucidatypewriter-Bold-R-*-*-*-130-*-*-*-*-iso8859-1"
 ;; (5) "-*-source code pro-Bold-R-*-*-*-140-*-*-*-*-iso8859-1"
 ;;=============================================================================
-(when (display-graphic-p)
-  (set-face-font
-   'default "-*-DejaVuSansMono-Bold-R-*-*-*-120-*-*-*-*-iso8859-1" ))
-
-;;=============================================================================
-;; Set variables to toggle various options on (t) or off (nil)
-;;=============================================================================
-(setq enable-two-vertical-frames t)   ;; Use larger window w/ 2 vertical frames
-(setq enable-vterm t)                 ;; Use vterm instead of term
+(unless enable-athena-backend
+  (when (display-graphic-p)
+    (set-face-font
+     'default "-*-DejaVuSansMono-Bold-R-*-*-*-120-*-*-*-*-iso8859-1")))
 
 ;;=============================================================================
 ;; Load emacs configurations from the emacs-config submodule
