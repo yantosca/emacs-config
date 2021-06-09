692cac8430d3b77c96bec59c980bd60145a43789
Bob Yantosca <yantosca@seas.harvard.edu>
2021-06-08 21:36:06 -0400

    Move files config files from user-options into elisp
    Signed-off-by: Bob Yantosca <yantosca@seas.harvard.edu>

--- user-options/frame-options-laptop.el
+++
@@ -1,8 +0,0 @@
-;;; frame-options-laptop.el -- Window & frame size for a laptop display
-;;
-;; 25 May 2021 -- Bob Yantosca -- yantosca@seas.harvard.edu
-
-(set-frame-height (selected-frame) 34)      ; 34 lines
-(set-frame-width  (selected-frame) 81)      ; 80 columns
-
-(provide 'frame-options-laptop)
